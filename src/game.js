9a1b3de35df1a326aa9faf0fe357a48cfcde14c1
thomasandrewmaclean <thomas.maclean@gmail.com>
2022-11-01 18:08:42 +0100

    cycle through the players

--- src/game.js
+++ src/game.js
@@ -1,4 +1,7 @@
-const state = {}
+const state = {
+    players: [],
+    currentPlayer: 0
+}
 
 
 // Screen1 Player selection
@@ -19,8 +22,26 @@ document.getElementById("addPlayer").addEventListener("click", () => {
 
 // Start game 
 document.getElementById("startGame").addEventListener("click", () => {
-    state.players = Array.from(document.querySelector(".addPlayersList").children).map(c => c.querySelector("input").value)
-
+    state.players = Array.from(document.querySelector(".addPlayersList").children).map(c => c.querySelector("input").value).filter(x => x)
+    setupPlayers()
     document.getElementById("setup").classList.add("hide");
     document.getElementById("game").classList.remove("hide")
 })
+
+// setup players
+const setupPlayers = () => {
+    const players = document.createElement("ul");
+    players.innerHTML = state.players.map((player, index) => `<li class="${index === 0 ? 'activePlayer player' : 'player'}">${player}</li>`).join("")
+    document.querySelector("footer").appendChild(players)
+    document.querySelector("footer").classList.remove("hide")
+}
+
+// next player
+const goToNextPlayer = () => {
+    state.currentPlayer = (state.currentPlayer + 1) % state.players.length;
+    document.querySelector(".activePlayer").classList.remove("activePlayer")
+    Array.from(document.querySelectorAll("footer li"))[state.currentPlayer].classList.add("activePlayer")
+}
+document.getElementById("nextPlayer").addEventListener("click",
+    goToNextPlayer)
+
